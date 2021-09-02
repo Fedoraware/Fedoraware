59f1ed1f9594a69bbd18f8c7e0eca47cc1c6102a
M-FeD <31779182+M-FeD@users.noreply.github.com>
2021-09-02 10:45:24 +0100

    Update EngineVGuiHook.cpp
    remove the 0 on screen lol

--- SE-Owned/TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
+++ SE-Owned/TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
@@ -141,8 +141,8 @@ void __stdcall EngineVGuiHook::Paint::Hook(int mode)
 							Color_t color2 = g_GlobalInfo.m_nWaitForShift ? Color_t{ 255, 134, 81, 180 } : Color_t{ 106, 255, 250, 180 };
 							//g_Draw.String(FONT_MENU, g_ScreenSize.c, nY - 100, { 255, 64, 64, 255 }, ALIGN_CENTERHORIZONTAL, _(L"Ticks Choked: %i "), ticksChoked);
 							//g_Draw.String(FONT_MENU, g_ScreenSize.c, nY - 100, { 255, 64, 64, 255 }, ALIGN_CENTERHORIZONTAL, Vars::Skybox::SkyboxName.c_str());
-							int chokemelikeyouhatemebutyoulovemelowkeywannadatemewhenyoufuckmeuwu = g_Interfaces.Engine->GetNetChannelInfo()->m_nChokedPackets;
-							g_Draw.String(FONT_MENU, g_ScreenSize.c, nY - 100, { 255, 64, 64, 255 }, ALIGN_CENTERHORIZONTAL, std::to_string(chokemelikeyouhatemebutyoulovemelowkeywannadatemewhenyoufuckmeuwu).data());
+							//int chokemelikeyouhatemebutyoulovemelowkeywannadatemewhenyoufuckmeuwu = g_Interfaces.Engine->GetNetChannelInfo()->m_nChokedPackets;
+							//g_Draw.String(FONT_MENU, g_ScreenSize.c, nY - 100, { 255, 64, 64, 255 }, ALIGN_CENTERHORIZONTAL, std::to_string(chokemelikeyouhatemebutyoulovemelowkeywannadatemewhenyoufuckmeuwu).data());
 							int tickWidth = 8;
 							int barWidth = (tickWidth * ticks) + 2;
 							g_Draw.OutlinedRect( // Outline of bar
