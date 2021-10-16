2cdba5768f15c40d5a943562336b6d1ec6a81172
oxyGodmode <87392051+oxyGodmode@users.noreply.github.com>
2021-10-16 20:27:17 +0300

    cya

--- SE-Owned/TeamFortress2/TeamFortress2/Hooks/Reset/Reset.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../SDK/SDK.h"
-
-namespace ResetHook {
-	using fn = HRESULT(__stdcall *)(IDirect3DDevice9 *, D3DPRESENT_PARAMETERS *);
-	HRESULT __stdcall Func(IDirect3DDevice9 *pDevice, D3DPRESENT_PARAMETERS *pPresentParams);
-	void Init();
-}
