55e3fd42576c59499d9a2feb2a76498e4678ca46
M-FeD <98985452+M-F3D@users.noreply.github.com>
2022-02-12 10:06:35 +0000

    Added ping reducer code if anyone wants to fix it...

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/EngineHook/EngineHook.h
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/EngineHook/EngineHook.h
@@ -27,4 +27,38 @@ namespace EngineHook
 		using fn = float(__fastcall*)(void*, void*);
 		float __fastcall Hook(void* ecx, void* edx);
 	}
+
+	namespace CL_ReadPackets
+	{
+		inline SEOHook::Func Func;
+		using fn = void(__cdecl*)(bool bFinalTick);
+		void __cdecl Hook(bool bFinalTick);
+	}
 }
+
+class CPingReducer
+{
+public:
+
+	struct ReadPacketsStore
+	{
+		float Frametime = 0.0f;
+		float CsFrametime = 0.0f;
+		float Curtime = 0.0f;
+		int   TickCount = 0;
+
+		void Setup();
+		void Apply() const;
+	};
+
+	ReadPacketsStore OnReadPacket;
+
+	bool ShouldCallReadPackets();
+
+	void FixInputDelay(bool a);
+
+	using ReadPacketFn = void(__cdecl*)(bool);
+	ReadPacketFn oReadPackets;
+};
+
+inline CPingReducer g_PingReducer;
--- Fedoraware/TeamFortress2/TeamFortress2/SDK/Interfaces/Interfaces.h
+++ Fedoraware/TeamFortress2/TeamFortress2/SDK/Interfaces/Interfaces.h
@@ -19,7 +19,6 @@
 #include "EngineVGui/EngineVGui.h"
 #include "NetChannel/NetChannel.h"
 #include "Steam/Steamworks.h"
-#include "Steam/SteamTypes.h"
 #include "GameEvent/GameEvent.h"
 #include "Material/Material.h"
 #include "ModelRender/ModelRender.h"
@@ -49,20 +48,35 @@ private:
 							// The server may be on a slightly different tick and the client will drift towards it.
 };
 
-class CClientState
-{
+class CClientState {
+private:
+	char pad_0000[8]; //0x0000
 public:
-	byte pad0[0x10];
-	INetChannel* m_NetChannel;			// 0x10
-	byte pad1[0x140];
-	CClockDriftMgr	m_ClockDriftMgr;		// 0x154
-	int				m_nDeltaTick;			// 0x1A0
-	byte pad2[0x110];
-	int				m_nMaxClients;			// 0x2B4	
-	byte pad3[0x486C];
-	int				lastoutgoingcommand;	// 0x4B24
-	int				chokedcommands;			// 0x4B28
-	int				last_command_ack;		// 0x4B2C
+	void* thisptr = nullptr; //0x0008
+private:
+	char pad_000C[4]; //0x000C
+public:
+	CNetChannel* m_NetChannel = nullptr; //0x0010
+private:
+	char pad_0014[320]; //0x0014
+public:
+	CClockDriftMgr m_ClockDriftMgr = { }; //0x0154
+	int m_nDeltaTick = 0; //0x01A0
+private:
+	char pad_01A4[16]; //0x01A4
+public:
+	char m_szLevelFileName[128]; //0x01B4
+	char m_szLevelBaseName[128]; //0x0234
+	int m_nMaxClients = 0; //0x02B4
+private:
+	char pad_02B8[18528]; //0x02B8
+public:
+	int	oldtickcount = 0;		//0x4B18
+	float m_tickRemainder = 0.f;	//0x4B1C
+	float m_frameTime = 0.f; //0x4B20
+	int lastoutgoingcommand = 0; //0x4B24
+	int chokedcommands = 0; //0x4B28
+	int last_command_ack = 0; //0x4B2C
 };
 
 class CSteamInterfaces
