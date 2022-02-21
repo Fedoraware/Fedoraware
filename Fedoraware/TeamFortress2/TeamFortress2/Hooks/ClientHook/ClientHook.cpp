0a696207aa6c016d9c03367b66270794e9a1b771
xxmyn <xxmynameisoliverxx@gmail.com>
2022-02-22 03:07:06 +1100

    Minor Fixes; - probably less problems

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/ClientHook/ClientHook.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/ClientHook/ClientHook.cpp
@@ -247,7 +247,9 @@ void __stdcall ClientHook::FrameStageNotify::Hook(EClientFrameStage FrameStage)
 				if (Vars::Visuals::Rain.m_Var > 0) {
 					g_Visuals.rain.Run();
 				}
-				g_Visuals.ModulateWorld(); // not sure if this is needed, test without it
+				static bool modded = false;
+				if (Vars::Visuals::SkyModulation.m_Var || Vars::Visuals::WorldModulation.m_Var) { g_Visuals.ModulateWorld(); modded = true; }
+				else if (modded) { modded = false; g_Visuals.ModulateWorld(); } // genius method i swear
 			}
 			break;
 		}
