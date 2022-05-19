49f4ced81ab9b37d15bcd2a4f48de105c1dd9da5
lnx00 <linch255@gmail.com>
2022-05-19 22:00:18 +0200

    :hammer: Properly updated crithack

--- Fedoraware/TeamFortress2/TeamFortress2/Features/CritHack/CritHack.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/CritHack/CritHack.cpp
@@ -31,7 +31,7 @@ bool CCritHack::ShouldCrit()
 }
 
 /* Returns the next crit command number */
-int CCritHack::NextCritTick(const CUserCmd* pCmd, int loops = 4096)
+int CCritHack::NextCritTick(const CUserCmd* pCmd, int loops = 16)
 {
 	static int previousWeapon = 0;
 	static int previousCrit = 0;
@@ -48,7 +48,7 @@ int CCritHack::NextCritTick(const CUserCmd* pCmd, int loops = 4096)
 	// Find the next crit tick
 	int foundTick = -1;
 	const int seedBackup = MD5_PseudoRandom(pCmd->command_number) & MASK_SIGNED;
-	for (int i = 0; i < 15; i++)
+	for (int i = 0; i < loops; i++)
 	{
 		const int cmdNum = pCmd->command_number + i;
 		*g_Interfaces.RandomSeed = MD5_PseudoRandom(cmdNum) & MASK_SIGNED;
