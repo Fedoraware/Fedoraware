8d9f9faf2413b547250f4f67fa08d53d1261a794
lnx00 <linch255@gmail.com>
2022-06-02 18:50:16 +0200

    :hammer: Added alternative even delay bypass

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/PatchManager/PatchManager.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/PatchManager/PatchManager.cpp
@@ -10,7 +10,9 @@ void CPatchManager::Init()
 
 	// Initialize all patches
 
-	// Add them to the patch vector
+	// https://github.com/OthmanAba/TeamFortress2/blob/1b81dded673d49adebf4d0958e52236ecc28a956/tf2_src/engine/cl_parse_event.cpp#L97
+	static BytePatch antiDelayedEvents{ g_Pattern.Find(L"engine.dll", L"7B 17 B9 ? ? ? ?"), { 0xEB }};
+	GetVecPatches().push_back(&antiDelayedEvents);
 }
 
 void CPatchManager::Restore()
