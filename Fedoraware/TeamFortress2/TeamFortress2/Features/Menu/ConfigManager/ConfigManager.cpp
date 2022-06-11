8d381bdffe76405986845ffd89b86ad95f50e016
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-06-11 17:40:44 +0100

    Add force medieval chat

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/ConfigManager/ConfigManager.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/ConfigManager/ConfigManager.cpp
@@ -703,6 +703,7 @@ bool CConfigManager::SaveConfig(const std::string& configName)
 			SAVE_VAR(Vars::Misc::PartyESP);
 			SAVE_VAR(Vars::Misc::Roll);
 			SAVE_VAR(Vars::Misc::SoundBlock);
+			SAVE_VAR(Vars::Misc::ForceMedievalChat);
 
 			// Followbot
 			{
@@ -1421,6 +1422,7 @@ bool CConfigManager::LoadConfig(const std::string& configName)
 			LOAD_VAR(Vars::Misc::PartyESP);
 			LOAD_VAR(Vars::Misc::Roll);
 			LOAD_VAR(Vars::Misc::SoundBlock);
+			LOAD_VAR(Vars::Misc::ForceMedievalChat);
 
 			// Followbot
 			{
