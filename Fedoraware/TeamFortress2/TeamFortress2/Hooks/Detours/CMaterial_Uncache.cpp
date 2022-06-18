0f51d85edc0d7620166c626c57bb781374ba3d44
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-06-18 19:56:18 +0100

    change some areas we use e8 to use the helper func

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CMaterial_Uncache.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CMaterial_Uncache.cpp
@@ -1,12 +1,6 @@
 #include "../Hooks.h"
 
-DWORD UncacheAddress()
-{
-	const DWORD dwAddress = g_Pattern.Find(L"MaterialSystem.dll", L"E8 ? ? ? ? 83 7E 1C 00 ") + 0x1;
-	return *reinterpret_cast<PDWORD>(dwAddress) + dwAddress + 4;
-}
-
-MAKE_HOOK(CMaterial_Uncache, UncacheAddress(), void, __fastcall,
+MAKE_HOOK(CMaterial_Uncache, g_Pattern.E8(L"MaterialSystem.dll", L"E8 ? ? ? ? 83 7E 1C 00 "), void, __fastcall,
 		  IMaterial* ecx, void* edx, bool bPreserveVars)
 {
 	if (ecx)
