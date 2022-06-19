9f9751375a7a190b7a8866bac3f85b8d30741396
lnx00 <linch255@gmail.com>
2022-06-19 21:08:17 +0200

    Small improvements/cleanup

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/NotificationQueue_Add.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/NotificationQueue_Add.cpp
@@ -5,7 +5,7 @@
 MAKE_HOOK(NotificationQueue_Add, g_Pattern.Find(L"client.dll", L"55 8B EC 8B 0D ? ? ? ? 56 57 8B 01 8B 40 68 FF D0 8B 75 08 84 C0 75 09 80 BE ? ? ? ? ? 75 29"), int, __cdecl,
 		  CEconNotification* pNotification)
 {
-	if (G::NotificationVector.size() > 0)
+	if (!G::NotificationVector.empty())
 	{
 		for (const auto& Notification : G::NotificationVector)
 		{
