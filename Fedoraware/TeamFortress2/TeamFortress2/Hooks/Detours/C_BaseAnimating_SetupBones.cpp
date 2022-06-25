1aa7d43654c8f7e6b721347ab728748b9de52614
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-06-25 08:58:40 +0100

    Rename hook and use a real signature

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/C_BaseAnimating_SetupBones.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/C_BaseAnimating_SetupBones.cpp
@@ -1,7 +1,7 @@
 #include "../Hooks.h"
 
 //	"C_BaseAnimating::SetupBones"
-MAKE_HOOK(CBaseAnimating_SetupBones, g_Pattern.E8(L"client.dll", L"E8 ? ? ? ? 80 BF ? ? ? ? ? 8A D8"), bool, __fastcall,
+MAKE_HOOK(C_BaseAnimating_SetupBones, g_Pattern.Find(L"client.dll", L"55 8B EC 81 EC ? ? ? ? 53 56 8B 35 ? ? ? ? 8B D9 33 C9 33 D2 89 4D EC 89 55 F0 8B 46 08 85 C0 74 3B"), bool, __fastcall,
 	void* eax, void* edi, matrix3x4* pBoneToWorldOut, int nMaxBones, int boneMask, float currentTime)
 {
 	return Hook.Original<FN>()(eax, edi, pBoneToWorldOut, nMaxBones, boneMask, currentTime);
