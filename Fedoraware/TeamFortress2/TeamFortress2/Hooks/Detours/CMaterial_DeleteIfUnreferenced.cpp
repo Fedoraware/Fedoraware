b2bcadcc8b7d1dfb6479a88d17a87e42c5f7a9b5
Baan <70382582+xxmyn@users.noreply.github.com>
2022-06-25 22:37:14 +1000

    Renamed hook.
    mfed is a bully.

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CMaterial_DeleteIfUnreferenced.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CMaterial_DeleteIfUnreferenced.cpp
@@ -1,6 +1,6 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(C_Material_DeleteIfUnreferenced, g_Pattern.Find(L"materialsystem.dll", L"56 8B F1 83 7E 1C 00 7F 51"), void, __fastcall,
+MAKE_HOOK(CMaterial_DeleteIfUnreferenced, g_Pattern.Find(L"materialsystem.dll", L"56 8B F1 83 7E 1C 00 7F 51"), void, __fastcall,
 	IMaterial* eax)
 {
 	if (eax) {
