3b7ac07a7611a5c1c497eea0888a1fcf17747770
Baan <70382582+xxmyn@users.noreply.github.com>
2022-06-25 15:12:02 +1000

    Sharpened hook.

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/ClientState_ProcessFixAngle.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/ClientState_ProcessFixAngle.cpp
@@ -1,4 +1,5 @@
 #include "../Hooks.h"
+#include "../../Features/Vars.h"
 
 //CClientState::ProcessFixAngle(SVC_FixAngle *)
 //CClientState::ProcessCrosshairAngle(SVC_CrosshairAngle *)
@@ -6,10 +7,7 @@
 //	"ProcessBSPDecal:"
 //	@servermsghandler.cpp L669
 MAKE_HOOK(ClientState_ProcessFixAngle, g_Pattern.Find(L"engine.dll", L"55 8B EC 8B 45 08 83 EC 08 F3 0F 10 15 ? ? ? ?"), bool, __fastcall,
-	void* CNetChan, void* edi, SVC_FixAngle* msg)
+	void* ecx, void* edx, SVC_FixAngle* msg)
 {
-	return false;
-	//msg->m_bRelative = true;
-	//msg->m_Angle = Vec3(0.f, 0.f, 0.f);
-	//return Hook.Original<FN>()(CNetChan, edi, msg);
+	return Vars::Visuals::PreventForcedAngles.Value ? false : Hook.Original<FN>()(ecx, edx, msg);
 }
