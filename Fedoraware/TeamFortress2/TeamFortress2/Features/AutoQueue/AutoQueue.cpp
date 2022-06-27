6e56001d8818d0cc2c615a4565bcd689d15ffb1d
Johnathon Walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-06-27 15:23:48 +0100

    unsure if this will fix auto accept casual match but pls try it yappa

--- Fedoraware/TeamFortress2/TeamFortress2/Features/AutoQueue/AutoQueue.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/AutoQueue/AutoQueue.cpp
@@ -12,9 +12,9 @@ void CAutoQueue::Run()
 		static auto CSharedObjectCache__FindBaseTypeCache = reinterpret_cast<int(__thiscall*)(void*, int)>(g_Pattern.Find(L"client.dll", L"55 8B EC 83 EC 08 8B 45 08 53 56 8B D9 89 45 F8 57 8D 45 F8 50 8D 4B 10 E8 ? ? ? ? 0F B7 F0 8B D6 3B 53 18 73 43 0F B7 43 26 B9 ? ? ? ? 66 3B C1 74 35"));
 		static auto CTFParty__SpewDebug = reinterpret_cast<void(__thiscall*)(void*)>(g_Pattern.Find(L"client.dll", L"55 8B EC 83 EC 68 A1 ? ? ? ? 53 8B D9 89 45 F0 8B 83 ? ? ? ? 8D 4D E8 56 8D B3 ? ? ? ? 89 5D F8 51 8B CE C7 45 ? ? ? ? ? C6 45 F4 00 FF 50 1C 8B C8 E8 ? ? ? ?"));
 
-		if (!I::TFPartyClient->BInStandbyQueue() &&
-			!I::TFGCClientSystem->BHaveLiveMatch() &&
-			I::TFGCClientSystem->GetNumMatchInvites() == 0)
+		if (!I::TFPartyClient->BInStandbyQueue() 	&&
+			!I::TFGCClientSystem->BHaveLiveMatch() 	&&
+			!I::TFGCClientSystem->GetNumMatchInvites())
 		{
 			I::TFPartyClient->LoadSavedCasualCriteria();
 			CCasualSidePanel__OnCommand("find_game");
@@ -24,7 +24,9 @@ void CAutoQueue::Run()
 	// Auto accept
 	if (Vars::Misc::AutoAccept.Value)
 	{
-		if (I::TFGCClientSystem->GetNumMatchInvites() > 0)
+		if (I::TFPartyClient->BInStandbyQueue() 	||
+			I::TFGCClientSystem->BHaveLiveMatch() 	||
+			I::TFGCClientSystem->GetNumMatchInvites())
 		{
 			I::TFGCClientSystem->JoinMMMatch();
 		}
