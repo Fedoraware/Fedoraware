d1129bfc6fb6a812ce5a1ea010339f840262ed51
Baan <70382582+xxmyn@users.noreply.github.com>
2022-06-29 22:45:59 +1000

    Fixed skybox tearing.
    Big credits to @lnx00  for telling me how to bytepatch this.
    Removed BaseClientDLL_VoiceStatus hook.

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/PatchManager/PatchManager.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/PatchManager/PatchManager.cpp
@@ -12,7 +12,9 @@ void CPatchManager::Init()
 
 	// https://github.com/OthmanAba/TeamFortress2/blob/1b81dded673d49adebf4d0958e52236ecc28a956/tf2_src/engine/cl_parse_event.cpp#L97
 	static BytePatch antiDelayedEvents{ g_Pattern.Find(L"engine.dll", L"7B 17 B9 ? ? ? ?"), { 0xEB }};
+	static BytePatch skyBoxFix{ g_Pattern.Find(L"engine.dll", L"0F 87 ? ? ? ? 8B 04 9D ? ? ? ? 8B 14 85 ? ? ? ? 85 D2 0F 84 ? ? ? ? 8B 06 "), {0x90, 0x90, 0x90, 0x90, 0x90, 0x90} };
 	GetVecPatches().push_back(&antiDelayedEvents);
+	GetVecPatches().push_back(&skyBoxFix);
 }
 
 void CPatchManager::Restore()
