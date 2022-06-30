f9e4d2788bc94309e145a7ae9652649879022ad3
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-06-30 19:35:13 +0100

    Better force f2p stuff

---
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/OpenVoiceMenu.cpp
@@ -0,0 +1,11 @@
+#include "../Hooks.h"
+
+MAKE_HOOK(OpenVoiceMenu, g_Pattern.Find(L"client.dll", L"55 8B EC 83 EC 64 56 E8 ? ? ? ?"), void, __cdecl,
+		  int index)
+{
+	if (I::TFGCClientSystem)
+	{
+		*((bool*)I::TFGCClientSystem + 1440) = false;
+	}
+	Hook.Original<FN>()(index);
+}
