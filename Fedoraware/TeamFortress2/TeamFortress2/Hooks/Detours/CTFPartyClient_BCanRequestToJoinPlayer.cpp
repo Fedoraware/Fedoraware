eeae726e307868df60885fa0ab8957b107e0050c
lnx00 <linch255@gmail.com>
2022-07-04 22:18:13 +0200

    Renamed Hook Parameters

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CTFPartyClient_BCanRequestToJoinPlayer.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CTFPartyClient_BCanRequestToJoinPlayer.cpp
@@ -1,7 +1,7 @@
 #include "../Hooks.h"
 
 MAKE_HOOK(CTFPartyClient_BCanRequestToJoinPlayer, g_Pattern.E8(L"client.dll", L"E8 ? ? ? ? 84 C0 74 42 E8 ? ? ? ? 8B C8"), bool, __fastcall,
-		  void* ecx, void* edx, int targetPlayer, int a3)
+		  CTFPartyClient* ecx, void* edx, int64 targetPlayer)
 {
 	return true;
 }
