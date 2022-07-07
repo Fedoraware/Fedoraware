61862d03b09f23568acb8b930799b6e2a1e75762
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-07-07 19:17:26 +0100

    Changed PaintTraverse to fastcall

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/Panel_PaintTraverse.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/Panel_PaintTraverse.cpp
@@ -3,46 +3,45 @@
 #include "../../Features/Visuals/Visuals.h"
 #include "../../Features/Camera/CameraWindow.h"
 
-MAKE_HOOK(Panel_PaintTraverse, Utils::GetVFuncPtr(I::Panel, 41), void, __stdcall,
-		  unsigned int vgui_panel, bool force_repaint, bool allow_force)
+MAKE_HOOK(Panel_PaintTraverse, Utils::GetVFuncPtr(I::Panel, 41), void, __fastcall,
+		  void* ecx, void* edx, unsigned int vgui_panel, bool force_repaint, bool allow_force)
 {
 	F::CameraWindow.Draw();
 	if (F::Visuals.RemoveScope(vgui_panel)) { return; }
-	
-	if (!I::Engine->IsInGame())
-	{
-		static auto UpdateInventory = reinterpret_cast<void(__thiscall*)(void*)>(g_Pattern.Find(L"client.dll", L"55 8B EC 81 EC ? ? ? ? 53 56 33 DB"));
-		static int delay = 0;
-		if (I::BackpackPanel)
-		{
-			delay++;
-			if (delay == 30000)
-			{
-				UpdateInventory(I::BackpackPanel);
-				static int count = 0;
-				count++;
 
-				if (count > 6) { count = 0; }
-				switch (count)
-				{
-					case 0: G::BackpackQuality = 22; break;
-					case 1: G::BackpackQuality = 11; break;
-					case 2: G::BackpackQuality = 6; break;
-					case 3: G::BackpackQuality = 1; break;
-					case 4: G::BackpackQuality = 3; break;
-					case 5: G::BackpackQuality = 20; break;
-					case 6: G::BackpackQuality = 21; break;
-				}
-
-			}
-
-			if (delay > 30000)
-			{
-				delay = 0;
-			}
-		}
-	}
-
-	// Forgive me father for I have sinned
-	Hook.Original<void(__thiscall*)(CPanel*, unsigned int, bool, bool)>()(I::Panel, vgui_panel, force_repaint, allow_force);
+	Hook.Original<FN>()(ecx, edx, vgui_panel, force_repaint, allow_force);
 }
+
+//if (!I::Engine->IsInGame())
+//{
+//	static auto UpdateInventory = reinterpret_cast<void(__thiscall*)(void*)>(g_Pattern.Find(L"client.dll", L"55 8B EC 81 EC ? ? ? ? 53 56 33 DB"));
+//	static int delay = 0;
+//	if (I::BackpackPanel)
+//	{
+//		delay++;
+//		if (delay == 30000)
+//		{
+//			UpdateInventory(I::BackpackPanel);
+//			static int count = 0;
+//			count++;
+//
+//			if (count > 6) { count = 0; }
+//			switch (count)
+//			{
+//				case 0: G::BackpackQuality = 22; break;
+//				case 1: G::BackpackQuality = 11; break;
+//				case 2: G::BackpackQuality = 6; break;
+//				case 3: G::BackpackQuality = 1; break;
+//				case 4: G::BackpackQuality = 3; break;
+//				case 5: G::BackpackQuality = 20; break;
+//				case 6: G::BackpackQuality = 21; break;
+//			}
+//
+//		}
+//
+//		if (delay > 30000)
+//		{
+//			delay = 0;
+//		}
+//	}
+//}
