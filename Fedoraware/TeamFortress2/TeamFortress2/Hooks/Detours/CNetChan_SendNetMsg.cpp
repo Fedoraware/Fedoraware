edc8b995ae809e330f7a9a1267e47079b0dffb16
lnx00 <linch255@gmail.com>
2022-07-08 15:32:53 +0200

    Fixed signatures for latest update

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CNetChan_SendNetMsg.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CNetChan_SendNetMsg.cpp
@@ -2,8 +2,8 @@
 
 //	"NetMsg"
 //	@net_chan.cpp L2524
-MAKE_HOOK(CNetChan_SendNetMsg, g_Pattern.Find(L"engine.dll", L"55 8B EC 56 8B F1 8D 8E ? ? ? ? E8 ? ? ? ? 85 C0 75 07"), bool, __fastcall,
-	void* CNetChan, void* edi, INetMessage& msg, bool bForceReliable, bool bVoice)
+MAKE_HOOK(CNetChan_SendNetMsg, g_Pattern.Find(L"engine.dll", L"55 8B EC 57 8B F9 8D 8F ? ? ? ? E8 ? ? ? ? 85 C0 75 07 B0 01 5F 5D C2 0C 00"), bool, __fastcall,
+	CNetChannel* netChannel, void* edi, INetMessage& msg, bool bForceReliable, bool bVoice)
 {
 	switch (msg.GetType()) {
 	case clc_VoiceData: {		// stop lag with voice chat
@@ -17,5 +17,5 @@ MAKE_HOOK(CNetChan_SendNetMsg, g_Pattern.Find(L"engine.dll", L"55 8B EC 56 8B F1
 	}
 	}
 
-	return Hook.Original<FN>()(CNetChan, edi, msg, bForceReliable, bVoice);
+	return Hook.Original<FN>()(netChannel, edi, msg, bForceReliable, bVoice);
 }
