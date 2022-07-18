a848d501ef6686632ce7ed2629ed8b4c63243145
Baan <70382582+xxmyn@users.noreply.github.com>
2022-07-18 20:10:03 +1000

    Added fakeangles to fakeangle chams (ikik)

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Visuals/FakeAngleManager/FakeAng.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Visuals/FakeAngleManager/FakeAng.cpp
@@ -1,5 +1,7 @@
 #include "FakeAng.h"
 
+//gets called after fakelag.
+
 void CFakeAng::Run(CUserCmd* pCmd) {
 	if (const auto& pLocal = g_EntityCache.GetLocal()) {
 		if (!pCmd) {
@@ -9,10 +11,35 @@ void CFakeAng::Run(CUserCmd* pCmd) {
 
 			if (const auto& pAnimState = pLocal->GetAnimState()) {
 
+				float flOldFrameTime = I::GlobalVars->frametime;
+				int nOldSequence = pLocal->m_nSequence();
+				float flOldCycle = pLocal->m_flCycle();
+				auto pOldPoseParams = pLocal->GetPoseParam();
+				char pOldAnimState[sizeof(CMultiPlayerAnimState)] = {};
+
+				memcpy(pOldAnimState, pAnimState, sizeof(CMultiPlayerAnimState));
+
+				auto Restore = [&]()
+				{
+					I::GlobalVars->frametime = flOldFrameTime;
+					pLocal->m_nSequence() = nOldSequence;
+					pLocal->m_flCycle() = flOldCycle;
+					pLocal->SetPoseParam(pOldPoseParams);
+					memcpy(pAnimState, pOldAnimState, sizeof(CMultiPlayerAnimState));
+				};
+
+				I::GlobalVars->frametime = 0.0f;
+
+				pAnimState->m_flCurrentFeetYaw = G::FakeViewAngles.y;
+				pAnimState->m_flGoalFeetYaw = G::FakeViewAngles.x;
+				pAnimState->Update(G::FakeViewAngles.y, G::FakeViewAngles.x);
+
 				matrix3x4 bones[128];
 				if (pLocal->SetupBones(bones, 128, BONE_USED_BY_ANYTHING, I::GlobalVars->curtime)) {
 					BoneMatrix = *reinterpret_cast<FakeMatrixes*>(bones);
 				}
+
+				Restore();
 			}
 		}
 
