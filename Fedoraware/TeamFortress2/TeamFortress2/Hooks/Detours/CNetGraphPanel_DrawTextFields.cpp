6b03756940a426f8b599f8361ff25dad866c5b3f
johnathon-walnut <36936588+johnathon-walnut@users.noreply.github.com>
2022-07-23 19:40:32 +0100

    Update CNetGraphPanel_DrawTextFields.cpp

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CNetGraphPanel_DrawTextFields.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CNetGraphPanel_DrawTextFields.cpp
@@ -3,5 +3,5 @@
 MAKE_HOOK(CNetGraphPanel_DrawTextFields, g_Pattern.Find(L"client.dll", L"55 8B EC A1 ? ? ? ? 81 EC ? ? ? ? 83 78 30 00 57 8B F9 0F 84 ? ? ? ? A1 ? ? ? ? 83 78 30 00 74 0B 8B 8F ? ? ? ? 89 4D FC EB 09 8B 87"), void, __fastcall,
 	void* ecx, void* edx, int graphvalue, int x, int y, int w, void* graph, void* cmdinfo)
 {
-	return Hook.Original<FN>()(eax, edi, graphvalue, x, y, w, graph, cmdinfo);
+	return Hook.Original<FN>()(ecx, edx, graphvalue, x, y, w, graph, cmdinfo);
 }
