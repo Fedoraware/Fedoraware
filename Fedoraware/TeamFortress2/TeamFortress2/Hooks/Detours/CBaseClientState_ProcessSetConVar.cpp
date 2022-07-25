a78a83c49d5ec8887e59488e12a67e36f3ae7332
Baan <70382582+xxmyn@users.noreply.github.com>
2022-07-25 15:59:44 +1000

    CBaseClientState_ProcessSetConVar
    Added debug messages

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClientState_ProcessSetConVar.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClientState_ProcessSetConVar.cpp
@@ -1,6 +1,6 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(CBaseClient_ProcessSetConVar, g_Pattern.Find(L"engine.dll", L"55 8B EC 83 EC 10 8B 45 08 53 8B D9 C7 45 ? ? ? ? ? 83 78 20 00 0F 8E ? ? ? ? 33 D2 56 89 55 F8 57"), bool, __fastcall,
+MAKE_HOOK(CBaseClientState_ProcessSetConVar, g_Pattern.Find(L"engine.dll", L"55 8B EC 8B 49 08 83 EC 0C 8B 01 8B 40 18 FF D0 84 C0 0F 85 ? ? ? ? 8B 45 08 53 33 DB 39 58 20 0F 8E ? ? ? ? 33 C9 56 89 4D FC 57 8B"), bool, __fastcall,
 	void* eax, void* edi, NET_SetConVar* msg)
 {
 	I::Cvar->ConsoleColorPrintf({ 255, 0, 0, 255 }, "%s\n", msg->ToString());
