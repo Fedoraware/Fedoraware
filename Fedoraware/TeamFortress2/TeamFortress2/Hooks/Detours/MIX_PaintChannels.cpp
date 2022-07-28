10d0ca583100af10dc09bbbd8812a6e10ed32230
Baan <70382582+xxmyn@users.noreply.github.com>
2022-07-28 21:02:10 +1000

    argh

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/MIX_PaintChannels.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/MIX_PaintChannels.cpp
@@ -1,7 +1,7 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(MIX_PaintChannels, g_Pattern.Find(L"engine.dll", L"55 8B EC 81 EC ? ? ? ? 8B 0D ? ? ? ? 53 33 DB 89 5D D0 89 5D D4 8B 01 89 5D D8 89 5D DC 85 C0 74 3C 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68"), void, __fastcall,
+MAKE_HOOK(MIX_PaintChannels, g_Pattern.Find(L"engine.dll", L"55 8B EC 81 EC ? ? ? ? 8B 0D ? ? ? ? 53 33 DB 89 5D D0 89 5D D4 8B 01 89 5D D8 89 5D DC 85 C0 74 3C 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68"), void, __cdecl,
 	void* ecx, void* edx, int endtime, bool bIsUnderwater)
 {
-	return Hook.Original<FN>()(ecx, edx, endtime, Vars::Visuals::RemoveScreenOverlays.Value ? false : bIsUnderwater);	//	maybe shouldn't use this bool, but this should remove the water sound if we are removing the overlay for it
+	Hook.Original<FN>()(ecx, edx, endtime, bIsUnderwater);
 }
