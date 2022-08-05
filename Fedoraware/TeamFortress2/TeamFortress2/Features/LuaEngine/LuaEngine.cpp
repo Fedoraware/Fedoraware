469ad7d29e361714b433ab6352272263472092d8
lnx00 <linch255@gmail.com>
2022-08-05 20:07:37 +0200

    Safety measures

--- Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/LuaEngine.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/LuaEngine.cpp
@@ -136,7 +136,7 @@ void CLuaEngine::Init()
 
 		// Input
 		auto inputTable = LuaState.create_named_table("Input");
-		inputTable["IsKeyDown"] = [](int key) { return (GetAsyncKeyState(key) & 0x8000) != 0; };
+		inputTable["IsDown"] = [](int key) { return (GetAsyncKeyState(key) & 0x8000) != 0; };
 		inputTable["GetMousePos"] = [] {
 			int x, y;
 			I::VGuiSurface->SurfaceGetCursorPos(x, y);
@@ -160,6 +160,36 @@ void CLuaEngine::Init()
 		fwareGlobals["ShouldShift"] = [] { return G::ShouldShift; };
 		fwareGlobals["CurrentTargetIdx"] = [] { return G::CurrentTargetIdx; };
 		fwareGlobals["GetPriority"] = [](uint32_t friendsId) { return G::PlayerPriority[friendsId].Mode; };
+
+		// Enums
+		auto enums = LuaState.create_named_table("Enums");
+		enums["Buttons"] = LuaState.create_table_with(
+			"IN_ATTACK", 1 << 0,
+			"IN_JUMP", 1 << 1,
+			"IN_DUCK", 1 << 2,
+			"IN_FORWARD", 1 << 3,
+			"IN_BACK", 1 << 4,
+			"IN_USE", 1 << 5,
+			"IN_CANCEL", 1 << 6,
+			"IN_LEFT", 1 << 7,
+			"IN_RIGHT", 1 << 8,
+			"IN_MOVELEFT", 1 << 9,
+			"IN_MOVERIGHT", 1 << 10,
+			"IN_ATTACK2", 1 << 11,
+			"IN_RUN", 1 << 12,
+			"IN_RELOAD", 1 << 13,
+			"IN_ALT1", 1 << 14,
+			"IN_ALT2", 1 << 15,
+			"IN_SCORE", 1 << 16,
+			"IN_SPEED", 1 << 17,
+			"IN_WALK", 1 << 18,
+			"IN_ZOOM", 1 << 19,
+			"IN_WEAPON1", 1 << 20,
+			"IN_WEAPON2", 1 << 21,
+			"IN_BULLRUSH", 1 << 22,
+			"IN_GRENADE2", 1 << 24,
+			"IN_ATTACK3", 1 << 25
+		);
 
 		// Interfaces
 		auto interfaceTable = LuaState.create_named_table("Interfaces");
--- Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/Wrapper/WBaseEntity.hpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/Wrapper/WBaseEntity.hpp
@@ -13,61 +13,73 @@ public:
 
 	int GetIndex()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetIndex();
 	}
 
 	Vec3 GetOrigin()
 	{
+		if (!IsValid()) { return { }; }
 		return BaseEntity->GetAbsOrigin();
 	}
 
 	int GetClassID()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetClientClass()->m_ClassID;
 	}
 
 	const char* GetClass()
 	{
+		if (!IsValid()) { return "Invalid"; }
 		return BaseEntity->GetClientClass()->GetName();
 	}
 
 	int GetHealth()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetHealth();
 	}
 
 	int GetAmmo()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetAmmo();
 	}
 
 	int GetFlags()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetFlags();
 	}
 
 	Vec3 GetEyePos()
 	{
+		if (!IsValid()) { return { }; }
 		return BaseEntity->GetEyePosition();
 	}
 
 	bool IsDormant()
 	{
+		if (!IsValid()) { return false; }
 		return BaseEntity->GetDormant();
 	}
 
 	bool IsAlive()
 	{
+		if (!IsValid()) { return false; }
 		return BaseEntity->IsAlive();
 	}
 
 	int GetTeam()
 	{
+		if (!IsValid()) { return 0; }
 		return BaseEntity->GetTeamNum();
 	}
 
 	void SetOrigin(const Vec3& v)
 	{
+		if (!IsValid()) { return; }
 		BaseEntity->SetAbsOrigin(v);
 		BaseEntity->SetVecOrigin(v);
 	}
