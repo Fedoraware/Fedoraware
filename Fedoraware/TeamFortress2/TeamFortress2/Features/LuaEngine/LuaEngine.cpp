de42f021da15da787d6276a6024395eab4594b5e
lnx00 <linch255@gmail.com>
2022-08-13 18:38:29 +0200

    Lua additions

--- Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/LuaEngine.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/LuaEngine/LuaEngine.cpp
@@ -169,9 +169,15 @@ void CLuaEngine::Init()
 		prClass["GetKills"] = &WPlayerResource::GetKills;
 		prClass["GetDeaths"] = &WPlayerResource::GetDeaths;
 		prClass["GetConnected"] = &WPlayerResource::GetConnected;
+		prClass["GetTeam"] = &WPlayerResource::GetTeam;
+		prClass["IsAlive"] = &WPlayerResource::IsAlive;
+		prClass["GetHealth"] = &WPlayerResource::GetHealth;
+		prClass["GetAccountID"] = &WPlayerResource::GetAccountID;
 		prClass["GetValid"] = &WPlayerResource::GetValid;
 		prClass["GetPlayerName"] = &WPlayerResource::GetPlayerName;
+		prClass["GetScore"] = &WPlayerResource::GetScore;
 		prClass["GetDamage"] = &WPlayerResource::GetDamage;
+		prClass["GetMaxHealth"] = &WPlayerResource::GetMaxHealth;
 
 		// UserMessage
 		auto userMsgClass = LuaState.new_usertype<WUserMessage>("UserMessage");
@@ -182,6 +188,7 @@ void CLuaEngine::Init()
 		userMsgClass["GetNumBitsLeft"] = &WUserMessage::GetNumBitsLeft;
 		userMsgClass["ReadByte"] = &WUserMessage::ReadByte;
 		userMsgClass["ReadFloat"] = &WUserMessage::ReadFloat;
+		userMsgClass["ReadLong"] = &WUserMessage::ReadLong;
 		userMsgClass["ReadString"] = &WUserMessage::ReadString;
 
 		// Draw
