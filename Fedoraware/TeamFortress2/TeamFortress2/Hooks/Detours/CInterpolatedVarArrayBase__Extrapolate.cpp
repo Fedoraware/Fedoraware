d1372a291642b09268e927a42771a99580a889d7
Baan <70382582+xxmyn@users.noreply.github.com>
2022-08-17 20:53:14 +1000

    removed some debug stuff.

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CInterpolatedVarArrayBase__Extrapolate.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CInterpolatedVarArrayBase__Extrapolate.cpp
@@ -3,6 +3,5 @@
 MAKE_HOOK(CInterpolatedVarArrayBase__Extrapolate, g_Pattern.Find(L"client.dll", L"55 8B EC 8B 45 0C 8B D1 F3 0F 10 05 ? ? ? ? 56"), void, __fastcall,
 	void* ecx, void* edx, void* pOut, void* pOld, void* pNew, float flDestinationTime, float flMaxExtrapolationAmount)
 {
-	if (Vars::Misc::DisableInterpolation.Value && Vars::Debug::DebugInfo.Value) {return;}
 	return Hook.Original<FN>()(ecx, edx, pOut, pOld, pNew, flDestinationTime, flMaxExtrapolationAmount);
 }
