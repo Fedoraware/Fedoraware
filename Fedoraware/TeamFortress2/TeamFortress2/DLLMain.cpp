35fda0cd2c7b383e2563f93d36959bc221fe2b79
AscendedSion <80121670+AscendedSion@users.noreply.github.com>
2022-09-08 19:53:26 +0100

    Update DLLMain.cpp

--- Fedoraware/TeamFortress2/TeamFortress2/DLLMain.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/DLLMain.cpp
@@ -52,7 +52,7 @@ void ShutdownRichPresence()
 
 void Loaded()
 {
-	I::Cvar->ConsoleColorPrintf({ 255, 193, 75, 255 }, "Fedoraware Loaded!\n");
+	I::Cvar->ConsoleColorPrintf({ 255, 193, 75, 255 }, "Fedoraware Loaded! (RIP Queen Elizabeth II)\n");
 	I::EngineClient->ClientCmd_Unrestricted("play vo/items/wheatley_sapper/wheatley_sapper_attached14.mp3");
 
 	const int dxLevel = g_ConVars.FindVar("mat_dxlevel")->GetInt();
