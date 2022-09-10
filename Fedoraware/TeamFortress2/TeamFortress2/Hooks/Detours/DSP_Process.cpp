fd4e1f3ecb833b4cafe7aec9f2e30b4f3b0c6d57
Baan <70382582+xxmyn@users.noreply.github.com>
2022-09-10 17:18:08 +1000

    hook

---
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/DSP_Process.cpp
@@ -0,0 +1,8 @@
+#include "../Hooks.h"
+
+MAKE_HOOK(DSP_Process, g_Pattern.Find(L"engine.dll", L"55 8B EC 51 56 8B 75 08 83 FE 1F 0F 87 ? ? ? ? 8B 0D ? ? ? ? 8B 01 8B 40 2C FF D0 84 C0 0F 84 ? ? ? ? A1 ? ? ? ? 6B F6 70 81 C6 ? ? ? ? 83 78 30 00 0F 85 ? ? ? ? 83"), void, __cdecl,
+	unsigned int idsp, int *pbfront, int *pbrear, int *pbcenter, int sampleCount)
+{
+	if (true){ return; }	//	skip DSP
+	Hook.Original<FN>()(idsp, pbfront, pbrear, pbcenter, sampleCount);
+}
--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/MIX_PaintChannels.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/MIX_PaintChannels.cpp
@@ -1,6 +1,6 @@
 #include "../Hooks.h"
 
-MAKE_HOOK(MIX_PaintChannels, g_Pattern.Find(L"engine.dll", L"55 8B EC 81 EC ? ? ? ? 8B 0D ? ? ? ? 53 33 DB 89 5D D0 89 5D D4 8B 01 89 5D D8 89 5D DC 85 C0 74 3C 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68"), void, __cdecl,
+MAKE_HOOK(MIX_PaintChannels, g_Pattern.Find(L"engine.dll", L"55 8B EC 81 EC ? ? ? ? 8B 0D ? ? ? ? 53 33 DB 89 5D D0 89 5D D4 8B 01 89 5D D8 89 5D DC 85 C0 74 3C 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68"), void, __fastcall,
 	void* ecx, void* edx, int endtime, bool bIsUnderwater)
 {
 	Hook.Original<FN>()(ecx, edx, endtime, bIsUnderwater);
