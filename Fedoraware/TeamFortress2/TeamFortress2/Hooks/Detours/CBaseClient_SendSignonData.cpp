7e016334ee041fb358335c4202ae84a64c0d1912
Baan <70382582+xxmyn@users.noreply.github.com>
2022-10-13 12:15:29 +1100

    fixed useless hook

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClient_SendSignonData.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClient_SendSignonData.cpp
@@ -2,7 +2,7 @@
 #include "../../Features/Chams/DMEChams.h"
 
 
-MAKE_HOOK(CBaseClient_SendSignonData, g_Pattern.Find(L"engine.dll", L"55 8B EC 83 EC 1C 56 68 ? ? ? ? 8B F1 FF 15 ? ? ? ? 83 C4 04 E8 ? ? ? ? 6A 14 8B C8 8B 10 FF 52 68 8B 86 ? ? ? ? 80 B8 ? ? ? ? ? 74 21 8B 80"), bool, __fastcall,
+MAKE_HOOK(CBaseClient_SendSignonData, g_Pattern.Find(L"engine.dll", L"55 8B EC 83 EC ? 56 68 ? ? ? ? 8B F1"), bool, __fastcall,
 	void* ecx, void* edx)
 {
 	F::DMEChams.CreateMaterials();
