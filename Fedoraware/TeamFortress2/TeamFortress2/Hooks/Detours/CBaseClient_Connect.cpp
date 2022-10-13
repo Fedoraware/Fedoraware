6cc2fab55999329be613ef3b3fc1a830ffea3bf8
Baan <70382582+xxmyn@users.noreply.github.com>
2022-10-13 12:17:20 +1100

    fixed useless hook

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClient_Connect.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/CBaseClient_Connect.cpp
@@ -2,7 +2,7 @@
 #include "../../Features/Chams/DMEChams.h"
 
 
-MAKE_HOOK(CBaseClient_Connect, g_Pattern.Find(L"engine.dll", L"55 8B EC 53 56 57 68 ? ? ? ? 8B F1 FF 15 ? ? ? ? 83 C4 04 E8 ? ? ? ? 6A 0E 8B C8 8B 10 FF 52 68 8B 06"), void, __fastcall,
+MAKE_HOOK(CBaseClient_Connect, g_Pattern.Find(L"engine.dll", L"55 8B EC 53 56 57 68 ? ? ? ? 8B F1"), void, __fastcall,
 	void* ecx, void* edx, const char* szName, int nUserID, INetChannel *pNetChannel, bool bFakePlayer, int clientChallenge)
 {
 	F::DMEChams.CreateMaterials();
