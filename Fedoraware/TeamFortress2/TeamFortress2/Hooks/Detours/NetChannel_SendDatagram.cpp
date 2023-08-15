311e9849d6291380b279c6776ef1f61468c15766
Jack North <70382582+xxmyn@users.noreply.github.com>
2023-08-16 08:04:51 +1000

    Fixed an issue with SendDatagram

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/NetChannel_SendDatagram.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/NetChannel_SendDatagram.cpp
@@ -4,13 +4,13 @@
 MAKE_HOOK(NetChannel_SendDatagram, g_Pattern.Find(L"engine.dll", L"55 8B EC B8 ? ? ? ? E8 ? ? ? ? A1 ? ? ? ? 53 56 8B D9"), int, __fastcall,
 		  INetChannel* netChannel, void* edx, bf_write* datagram)
 {
-	if (!netChannel || datagram || !Vars::Backtrack::Enabled.Value || !Vars::Backtrack::Latency.Value) {
+	if (!netChannel || datagram) {
 		return Hook.Original<FN>()(netChannel, edx, datagram);
 	}
 
 	CBaseEntity* pLocal = g_EntityCache.GetLocal();
 
-	if (!pLocal || !pLocal->IsAlive() || pLocal->IsAGhost()) {
+	if (!pLocal || !pLocal->IsAlive() || pLocal->IsAGhost() || !Vars::Backtrack::Enabled.Value || !Vars::Backtrack::Latency.Value) {
 		F::Backtrack.bFakeLatency = false;
 		return Hook.Original<FN>()(netChannel, edx, datagram);
 	}
