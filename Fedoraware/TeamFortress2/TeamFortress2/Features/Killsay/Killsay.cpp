ef8fb13b27c00be3d2a2e2c7a87f60ffcaf23560
GC634 <110097220+GC634@users.noreply.github.com>
2023-08-19 22:03:25 +0200

    Better kill says
    Still shit.

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Killsay/Killsay.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Killsay/Killsay.cpp
@@ -1,160 +1,142 @@
-#include "Killsay.h"
 
-#include <fstream>
-#include <filesystem>
-#include <boost/algorithm/string/replace.hpp>
+#include <settings/Int.hpp>
+#include "common.hpp"
 
-const char* ClassToString(const int nClass)
+namespace hacks::shared::killsay
 {
-	switch (nClass)
-	{
-		default:
-		{
-			return "Unknown";
-		}
-		case CLASS_SCOUT:
-		{
-			return "Scout";
-		}
-		case CLASS_SNIPER:
-		{
-			return "Sniper";
-		}
-		case CLASS_SOLDIER:
-		{
-			return "Soldier";
-		}
-		case CLASS_DEMOMAN:
-		{
-			return "Demoman";
-		}
-		case CLASS_MEDIC:
-		{
-			return "Medic";
-		}
-		case CLASS_HEAVY:
-		{
-			return "Heavy";
-		}
-		case CLASS_PYRO:
-		{
-			return "Pyro";
-		}
-		case CLASS_SPY:
-		{
-			return "Spy";
-		}
-		case CLASS_ENGINEER:
-		{
-			return "Engineer";
-		}
-	}
+static settings::Int killsay_mode{ "killsay.mode", "0" };
+static settings::String filename{ "killsay.file", "killsays.txt" };
+static settings::Int delay{ "killsay.delay", "100" };
+
+struct KillsayStorage
+{
+    Timer timer{};
+    unsigned delay{};
+    std::string message{};
+};
+
+static boost::unordered_flat_map<int, KillsayStorage> killsay_storage{};
+
+// Thanks HellJustFroze for linking me http://daviseford.com/shittalk/
+const std::vector<std::string> builtin_default = { "Don't worry guys, I'm a garbage collector. I'm used to carrying trash.", "%name% is the human equivalent of a participation award.", "I would insult %name%, but nature did a better job.", "%name%, perhaps your strategy should include trying.", "Some people get paid to suck, you do it for free, %name%.", "You must really like that respawn timer, %name%.", "If your main is %class%, you should give up.", "Hey %name%, i see you can't play %class%. Try quitting the game.", "%name%@gmail.com to vacreview@valvesoftware.com\nFOUND CHEATER", "\n☐ Not rekt\n ☑ Rekt\n ☑ Really Rekt\n ☑ Tyrannosaurus Rekt" };
+
+const std::vector<std::string> builtin_nonecore_mlg = { "GET REKT U SCRUB", "GET REKT M8", "U GOT NOSCOPED M8", "U GOT QUICKSCOPED M8", "2 FAST 4 U, SCRUB", "U GOT REKT, M8" };
+const std::string tf_classes_killsay[]              = { "class", "scout", "sniper", "soldier", "demoman", "medic", "heavy", "pyro", "spy", "engineer" };
+const std::string tf_teams_killsay[]                = { "RED", "BLU" };
+static std::string lastmsg{};
+
+TextFile file{};
+
+std::string ComposeKillSay(IGameEvent *event)
+{
+    const std::vector<std::string> *source = nullptr;
+    switch (*killsay_mode)
+    {
+    case 1:
+        source = &file.lines;
+        break;
+    case 2:
+    case 3:
+        source = &builtin_default;
+        break;
+    case 4:
+        source = &builtin_nonecore_mlg;
+        break;
+    default:
+        break;
+    }
+    if (!source || source->empty())
+        return "";
+    if (!event)
+        return "";
+    int vid = event->GetInt("userid");
+    int kid = event->GetInt("attacker");
+    if (kid == vid)
+        return "";
+    if (GetPlayerForUserID(kid) != g_IEngine->GetLocalPlayer())
+        return "";
+    std::string msg = source->at(rand() % source->size());
+    //	checks if the killsays.txt file is not 1 line. 100% sure it's going
+    // to crash if it is.
+    while (msg == lastmsg && source->size() > 1)
+        msg = source->at(rand() % source->size());
+    lastmsg = msg;
+    player_info_s info{};
+    GetPlayerInfo(GetPlayerForUserID(vid), &info);
+
+    ReplaceSpecials(msg);
+    CachedEntity *ent = ENTITY(GetPlayerForUserID(vid));
+    int clz           = g_pPlayerResource->GetClass(ent);
+    ReplaceString(msg, "%class%", tf_classes_killsay[clz]);
+    player_info_s infok{};
+    GetPlayerInfo(GetPlayerForUserID(kid), &infok);
+    ReplaceString(msg, "%killer%", std::string(infok.name));
+    ReplaceString(msg, "%team%", tf_teams_killsay[ent->m_iTeam() - 2]);
+    ReplaceString(msg, "%myteam%", tf_teams_killsay[LOCAL_E->m_iTeam() - 2]);
+    ReplaceString(msg, "%myclass%", tf_classes_killsay[g_pPlayerResource->GetClass(LOCAL_E)]);
+    ReplaceString(msg, "%name%", std::string(info.name));
+    return msg;
 }
 
-void CKillsay::FireGameEvent(FNV1A_t uNameHash, CGameEvent* pEvent)
+class KillSayEventListener : public IGameEventListener2
 {
-        if (!Vars::Misc::Killsay.Value) 
+    void FireGameEvent(IGameEvent *event) override
+    {
+        if (!killsay_mode)
+            return;
+        std::string message = hacks::shared::killsay::ComposeKillSay(event);
+        if (!message.empty())
         {
-                return;
+            int vid                    = event->GetInt("userid");
+            killsay_storage[vid].delay = *delay;
+            killsay_storage[vid].timer.update();
+            killsay_storage[vid].message = message;
         }
+    }
+};
 
-	if (Vars::Misc::KillsayFile.empty())
-	{
-		return;
-	}
-
-	if (!m_bFilled)
-	{
-		m_vecLoadedKillsays.clear();
-
-		const std::string sCurrentPath = std::filesystem::current_path().string() + "\\FedFigs";
-
-		if (!std::filesystem::exists(sCurrentPath))
-		{
-			std::filesystem::create_directory(sCurrentPath);
-		}
-
-		if (!std::filesystem::exists(sCurrentPath + "\\Killsays"))
-		{
-			std::filesystem::create_directory(sCurrentPath + "\\Killsays");
-		}
-
-		std::string sKillsayPath = (sCurrentPath + "\\Killsays\\" + Vars::Misc::KillsayFile);
-
-		std::ifstream killsayFile(sKillsayPath);
-
-		std::string currentKillsay;
-
-		while (std::getline(killsayFile, currentKillsay))
-		{
-			if (currentKillsay.size() > 0)
-			{
-				m_vecLoadedKillsays.push_back(currentKillsay);
-			}
-		}
-
-		if (m_vecLoadedKillsays.size())
-		{
-			m_bFilled = true;
-		}
-		else
-		{
-			Vars::Misc::KillsayFile.clear();
-			return;
-		}
-	}
-
-	if (m_vecLoadedKillsays.empty())
-	{
-		return;
-	}
-
-	if (uNameHash == FNV1A::HashConst("player_death"))
-	{
-		const int attacker = Utils::GetPlayerForUserID(pEvent->GetInt("attacker"));
-		const int userid = Utils::GetPlayerForUserID(pEvent->GetInt("userid"));
-
-		if (userid == I::EngineClient->GetLocalPlayer())
-		{
-			return;
-		}
-
-		if (attacker != I::EngineClient->GetLocalPlayer())
-		{
-			return;
-		}
-
-		// Lol lets hope u dont have medal flip on and 9 lines of killsay
-
-		const auto& pEntity = I::ClientEntityList->GetClientEntity(userid);
-
-		if (!pEntity)
-		{
-			return;
-		}
-
-		if (!pEntity->IsPlayer())
-		{
-			return;
-		}
-
-		PlayerInfo_t pi = {};
-
-		if (!I::EngineClient->GetPlayerInfo(userid, &pi))
-		{
-			return;
-		}
-
-		int nSelectedKillsay = I::UniformRandomStream->RandomInt(0, m_vecLoadedKillsays.size() - 1);
-		std::string selectedKillsay = m_vecLoadedKillsays.at(nSelectedKillsay);
-
-		const char* szName = pi.name;
-		int nClass = pEntity->GetClassNum();
-
-		boost::replace_all(selectedKillsay, "%victim%", szName);
-		boost::replace_all(selectedKillsay, "%class%", ClassToString(nClass));
-
-		const std::string cmd = "say \"" + selectedKillsay + "\"";
-		I::EngineClient->ServerCmd(cmd.c_str(), true);
-	}
+static void ProcessKillsay()
+{
+    if (killsay_storage.empty())
+        return;
+    for (auto &i : killsay_storage)
+    {
+        if (i.second.message.empty())
+            continue;
+        if (i.second.timer.test_and_set(i.second.delay))
+        {
+            chat_stack::Say(i.second.message, false);
+            i.second = {};
+        }
+    }
 }
+
+static KillSayEventListener listener{};
+
+void reload()
+{
+    file.Load(*filename);
+}
+
+static CatCommand reload_command("killsay_reload", "Reload killsays", []() { reload(); });
+
+void init()
+{
+    reload();
+    g_IEventManager2->AddListener(&listener, (const char *) "player_death", false);
+}
+
+void shutdown()
+{
+    g_IEventManager2->RemoveListener(&listener);
+}
+
+static InitRoutine runinit(
+    []()
+    {
+        EC::Register(EC::Paint, ProcessKillsay, "paint_killsay", EC::average);
+        EC::Register(EC::Shutdown, shutdown, "shutdown_killsay", EC::average);
+        init();
+    });
+
+} // namespace hacks::shared::killsay
