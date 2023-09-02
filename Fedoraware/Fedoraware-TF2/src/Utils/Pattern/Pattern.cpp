bf863777517e9884e7fcc2ec8d2a8cbaee9a55c4
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-09-02 23:02:11 +0200

    Added alternative pattern finder

--- Fedoraware/Fedoraware-TF2/src/Utils/Pattern/Pattern.cpp
+++ Fedoraware/Fedoraware-TF2/src/Utils/Pattern/Pattern.cpp
@@ -1,6 +1,7 @@
 #include "Pattern.h"
 
 #include <thread>
+#include <vector>
 
 #define INRANGE(x,a,b)	(x >= a && x <= b)
 #define GET_BITS(x)		(INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
@@ -77,4 +78,87 @@ DWORD CPattern::E8(LPCWSTR szModuleName, LPCWSTR szPattern)
 {
 	DWORD dwAddress = Find(szModuleName, szPattern) + 0x1;
 	return *reinterpret_cast<PDWORD>(dwAddress) + dwAddress + 4;
+}
+
+
+
+std::vector<int> PatternToBytes(LPCSTR szPattern)
+{
+	std::vector<int> bytes;
+	const auto start = const_cast<LPSTR>(szPattern);
+	const auto end = start + strlen(szPattern);
+
+	for (auto pCur = start; pCur < end; ++pCur) {
+		if (*pCur == '?')
+		{
+			++pCur;
+			if (*pCur == '?')
+			{
+				++pCur;
+			}
+
+			bytes.push_back(-1);
+		}
+		else
+		{
+			bytes.push_back(strtoul(pCur, &pCur, 16));
+		}
+	}
+
+	return bytes;
+}
+
+DWORD CPattern::FindPattern(const DWORD& dwAddress, const DWORD& dwLength, LPCSTR szPattern)
+{
+	const auto patternBytes = PatternToBytes(szPattern);
+	if (patternBytes.empty()) { return 0; }
+
+	unsigned curMatch = 0u;
+	DWORD dwFirstMatch = 0;
+	for (auto pCur = dwAddress; pCur < dwLength; pCur++)
+	{
+		if (patternBytes[curMatch] == -1 || *reinterpret_cast<BYTE*>(pCur) == patternBytes[curMatch])
+		{
+			if (!dwFirstMatch)
+			{
+				dwFirstMatch = pCur;
+			}
+
+			if (++curMatch == patternBytes.size())
+			{
+				return dwFirstMatch;
+			}
+		}
+		else
+		{
+			curMatch = 0;
+			dwFirstMatch = 0;
+		}
+	}
+
+	return 0;
+}
+
+HMODULE CPattern::GetModuleHandleSafe(LPCSTR szModuleName)
+{
+	HMODULE hModule = nullptr;
+
+	while (!hModule)
+	{
+		hModule = GetModuleHandleA(szModuleName);
+		Sleep(250);
+	}
+
+	return hModule;
+}
+
+DWORD CPattern::Find(LPCSTR szModuleName, LPCSTR szPattern)
+{
+	const auto modHandle = reinterpret_cast<DWORD>(GetModuleHandleSafe(szModuleName));
+	if (!modHandle) { return 0x0; }
+
+	const auto ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(modHandle + reinterpret_cast<PIMAGE_DOS_HEADER>(modHandle)->e_lfanew);
+	if (!ntHeaders) { return 0x0; }
+
+	return FindPattern(modHandle + ntHeaders->OptionalHeader.BaseOfCode, modHandle + ntHeaders->OptionalHeader.SizeOfCode, szPattern);
 }
--- Fedoraware/Fedoraware-TF2/src/Utils/Pattern/Pattern.h
+++ Fedoraware/Fedoraware-TF2/src/Utils/Pattern/Pattern.h
@@ -7,10 +7,17 @@ class CPattern
 	DWORD FindPattern(DWORD dwAddress, DWORD dwLength, LPCWSTR szPattern);
 	HMODULE GetModuleHandleSafe(LPCWSTR szModuleName);
 
+	DWORD FindPattern(const DWORD& dwAddress, const DWORD& dwLength, LPCSTR szPattern);
+	HMODULE GetModuleHandleSafe(LPCSTR szModuleName);
+
 public:
 	DWORD Find(LPCWSTR szModuleName, LPCWSTR szPattern);
 	// Cry about it myzarfin
 	DWORD E8(LPCWSTR szModuleName, LPCWSTR szPattern);
+
+	// New
+	DWORD Find(LPCSTR szModuleName, LPCSTR szPattern);
+
 };
 
 inline CPattern g_Pattern;
