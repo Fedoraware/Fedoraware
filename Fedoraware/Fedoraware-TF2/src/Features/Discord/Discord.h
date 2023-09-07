f950a3d1a1b266e554c61de7c6eb1560475e4d07
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-09-07 20:29:37 +0200

    Dynamically load discord rpc

--- Fedoraware/Fedoraware-TF2/src/Features/Discord/Discord.h
+++ Fedoraware/Fedoraware-TF2/src/Features/Discord/Discord.h
@@ -4,12 +4,16 @@
 class CDiscordRPC
 {
 	bool m_IsActive = false;
+	bool m_Loaded = false;
 
 public:
+	CDiscordRPC();
+
 	void Init();
 	void Shutdown();
-
 	void Update();
+
+	bool IsLoaded() { return m_Loaded; }
 };
 
 ADD_FEATURE(CDiscordRPC, DiscordRPC)
