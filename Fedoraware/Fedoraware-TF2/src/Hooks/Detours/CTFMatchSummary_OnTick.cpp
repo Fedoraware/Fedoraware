8b6122d58c2ebed30e9ba0470f342d453c2abcfe
Jack North <70382582+xxmyn@users.noreply.github.com>
2023-09-14 23:11:15 +1000

    Crash Fix.

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CTFMatchSummary_OnTick.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CTFMatchSummary_OnTick.cpp
@@ -3,7 +3,8 @@
 // Credits: mfed
 MAKE_HOOK(CTFMatchSummary_OnTick, S::CTFMatchSummary_OnTick(), int, __fastcall, void* ecx, void* edx)
 {
-	auto* flags = reinterpret_cast<DWORD*>(I::TFGCClientSystem + 1488);
+    if (!reinterpret_cast<DWORD*>(ecx) + 750) { return Hook.Original<FN>()(ecx, edx); } //  m_iCurrentState == MS_STATE_INITIAL
+	DWORD* flags = reinterpret_cast<DWORD*>(I::TFGCClientSystem + 1488);
     if (flags && *flags & 2)
     {
         *flags &= ~2;
