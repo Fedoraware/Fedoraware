6a86ff2b056bae3e098ec7be9bc3ba24ed05d68d
PiantaMK <113540867+PiantaMK@users.noreply.github.com>
2023-09-30 12:23:51 +0200

    Revert "show rage priority (braindead solution)"
    This reverts commit 34040efb130f0b4ec55bda44c8e982c0d8165ca2.

--- Fedoraware/Fedoraware-TF2/src/Features/ESP/ESP.cpp
+++ Fedoraware/Fedoraware-TF2/src/Features/ESP/ESP.cpp
@@ -399,13 +399,6 @@ void CESP::DrawPlayers(CBaseEntity* pLocal)
 					nTextOffset += FONT.nTall;
 				}
 
-				// Rage flagged ESP
-				if (G::PlayerPriority[pi.friendsID].Mode == 3 && Vars::ESP::Players::CheaterDetection.Value)
-				{
-					g_Draw.String(FONT, middle, y - 28, { 255, 255, 0, 255 }, ALIGN_CENTERHORIZONTAL, "RAGE");
-					nTextOffset += FONT.nTall;
-				}
-
 				// GUID ESP
 				if (Vars::ESP::Players::GUID.Value)
 				{
