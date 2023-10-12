f785c4aa5d4962df9f578117cce7e181734cbfbb
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-10-13 00:57:18 +0200

    Minor camera window cleanup

--- Fedoraware/Fedoraware-TF2/src/Features/Camera/CameraWindow.cpp
+++ Fedoraware/Fedoraware-TF2/src/Features/Camera/CameraWindow.cpp
@@ -3,13 +3,91 @@
 #include "../../Hooks/HookManager.h"
 #include "../../Hooks/Hooks.h"
 
+void CCameraWindow::SpyCamera(CBaseEntity* pLocal)
+{
+	// Spy camera
+	for (const auto& player : g_EntityCache.GetGroup(EGroupType::PLAYERS_ENEMIES))
+	{
+		if (!player->IsAlive())
+		{
+			continue;
+		}
+
+		if (player->GetClassNum() != CLASS_SPY)
+		{
+			continue;
+		}
+
+		if (pLocal->GetAbsOrigin().DistTo(player->GetAbsOrigin()) <= 350.f)
+		{
+			CameraOrigin = player->GetEyePosition();
+			CameraAngles = player->GetEyeAngles();
+			CanDraw = true;
+			return;
+		}
+	}
+
+	CanDraw = false;
+}
+
+void CCameraWindow::TeleCamera(CBaseEntity* pLocal)
+{
+	// Teleporter camera
+	const auto& buildings = g_EntityCache.GetGroup(EGroupType::BUILDINGS_ALL);
+
+	for (const auto& pBuilding : buildings)
+	{
+		if (!pBuilding->IsAlive()) { continue; }
+
+		const auto& building = reinterpret_cast<CBaseObject*>(pBuilding);
+		const auto nType = static_cast<EBuildingType>(building->GetType());
+
+		const float distance = pLocal->GetAbsOrigin().DistTo(building->GetAbsOrigin());
+		if (distance > 260.f) { continue; }
+
+		// Is it a teleporter?
+		if (nType != EBuildingType::TELEPORTER || building->GetObjectMode() != 0) { continue; }
+
+		// Get the teleporter exit
+		Vec3 tpExit;
+		if (!Utils::GetTeleporterExit(building->GetOwner()->GetIndex(), &tpExit)) { continue; }
+
+		CameraAngles = I::EngineClient->GetViewAngles();
+		CameraOrigin = Vec3(tpExit.x, tpExit.y, tpExit.z + 70);
+		CanDraw = true;
+
+		// Portal mode
+		if (Vars::Visuals::CameraMode.Value == 4)
+		{
+			Vec3 vScreen;
+			if (Utils::W2S(building->GetAbsOrigin(), vScreen))
+			{
+				const float height = Math::RemapValClamped(100.f, 0.f, distance, 100.f, 600.f);
+				const float width = Math::RemapValClamped(100.f, 0.f, distance, 80.f, 400.f);
+				ViewRect.w = static_cast<int>(width);
+				ViewRect.h = static_cast<int>(height);
+				ViewRect.x = static_cast<int>(vScreen.x - width * 0.5);
+				ViewRect.y = static_cast<int>(vScreen.y - height);
+			}
+			else
+			{
+				CanDraw = true;
+			}
+		}
+
+		return;
+	}
+
+	CanDraw = false;
+}
+
 void CCameraWindow::Init()
 {
 	// Create camera texture
 	CameraTex = I::MaterialSystem->CreateFullFrameRenderTarget("mirrorcam_rt");
 
 	// Create camera material
-	const auto kv = new KeyValues("UnlitGeneric");
+	static auto* kv = new KeyValues("UnlitGeneric");
 	kv->SetString("$basetexture", "mirrorcam_rt");
 	CameraMat = I::MaterialSystem->Create("m_cameraMat", kv);
 }
@@ -40,81 +118,20 @@ void CCameraWindow::Draw()
 void CCameraWindow::Update()
 {
 	if (Vars::Visuals::CameraMode.Value <= 1) { return; }
-	if (const auto& pLocal = g_EntityCache.GetLocal())
+
+	const auto pLocal = g_EntityCache.GetLocal();
+	if (!pLocal) { return; }
+
+	switch (Vars::Visuals::CameraMode.Value)
 	{
-		switch (Vars::Visuals::CameraMode.Value)
-		{
-			case 2:
-			{
-				// Spy camera
-				for (const auto& player : g_EntityCache.GetGroup(EGroupType::PLAYERS_ENEMIES))
-				{
-					if (!player->IsAlive())
-						continue;
+	case 2:
+		SpyCamera(pLocal);
+		break;
 
-					if (player->GetClassNum() != CLASS_SPY)
-						continue;
-
-					if (pLocal->GetAbsOrigin().DistTo(player->GetAbsOrigin()) <= 350.f)
-					{
-						CameraOrigin = player->GetEyePosition();
-						CameraAngles = player->GetEyeAngles();
-						CanDraw = true;
-						return;
-					}
-				}
-				CanDraw = false;
-				break;
-			}
-
-			case 3:
-			case 4:
-			{
-				// Teleporter camera
-				const auto& buildings = g_EntityCache.GetGroup(EGroupType::BUILDINGS_ALL);
-
-				for (const auto& pBuilding : buildings)
-				{
-					if (!pBuilding->IsAlive()) { continue; }
-
-					const auto& building = reinterpret_cast<CBaseObject*>(pBuilding);
-					const auto nType = static_cast<EBuildingType>(building->GetType());
-
-					const float distance = pLocal->GetAbsOrigin().DistTo(building->GetAbsOrigin());
-					if (distance > 260.f) { continue; }
-
-					if (nType == EBuildingType::TELEPORTER && building->GetObjectMode() == 0)
-					{
-						Vec3 tpExit;
-						if (Utils::GetTeleporterExit(building->GetOwner()->GetIndex(), &tpExit))
-						{
-							CameraAngles = I::EngineClient->GetViewAngles();
-							CameraOrigin = Vec3(tpExit.x, tpExit.y, tpExit.z + 70);
-							CanDraw = true;
-
-							if (Vars::Visuals::CameraMode.Value == 4)
-							{
-								Vec3 vScreen;
-								if (Utils::W2S(building->GetAbsOrigin(), vScreen))
-								{
-									const float height = Math::RemapValClamped(100.f, 0.f, distance, 100.f, 600.f);
-									const float width = Math::RemapValClamped(100.f, 0.f, distance, 80.f, 400.f);
-									ViewRect.w = width;
-									ViewRect.h = height;
-									ViewRect.x = vScreen.x - (width * 0.5);
-									ViewRect.y = vScreen.y - height;
-								}
-								else { CanDraw = true; }
-							}
-
-							return;
-						}
-					}
-				}
-
-				CanDraw = false;
-			}
-		}
+	case 3:
+	case 4:
+		TeleCamera(pLocal);
+		break;
 	}
 }
 
@@ -132,15 +149,15 @@ void CCameraWindow::RenderView(void* ecx, const CViewSetup& pViewSetup)
 	mirrorView.y = 0;
 	if (Vars::Visuals::CameraMode.Value != 1)
 	{
-// Custom origin & angles
+		// Custom origin & angles
 		mirrorView.origin = CameraOrigin;
 		mirrorView.angles = CameraAngles;
 	}
 	else
 	{
-  // Mirror cam
+		// Mirror cam
 		const Vec3 viewAngles = I::EngineClient->GetViewAngles();
-		const Vec3 camAngles = { 0.f, viewAngles.y + 180.f, viewAngles.z };
+		const Vec3 camAngles = {0.f, viewAngles.y + 180.f, viewAngles.z};
 		mirrorView.angles = camAngles;
 	}
 	mirrorView.width = ViewRect.w;
