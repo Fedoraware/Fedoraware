1c412393b80e43069975b0953a1d9527c13591eb
lnx00 <34384416+lnx00@users.noreply.github.com>
2023-10-13 01:09:51 +0200

    Renamed duplicate hook name

--- Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CEconNotification_HasNewItems.cpp
+++ Fedoraware/Fedoraware-TF2/src/Hooks/Detours/CEconNotification_HasNewItems.cpp
@@ -2,7 +2,7 @@
 
 #include "../../SDK/Main/CEconNotification.h"
 
-MAKE_HOOK(CEconNotification_HasNewItems_CEconNotification_HasNewItems, S::CEconNotification_HasNewItems(), void*, __fastcall,
+MAKE_HOOK(CEconNotification_HasNewItems, S::CEconNotification_HasNewItems(), void*, __fastcall,
 		  void* ecx, void* edx)
 {
 	auto* pNotification = static_cast<CEconNotification*>(Hook.Original<FN>()(ecx, edx));
